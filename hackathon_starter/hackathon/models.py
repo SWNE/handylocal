549503e8dd9a10e1d0777e914fea68691613348d
Vukosi Marivate <vmarivate@csir.co.za>
2015-06-27 12:55:56 +0200

    Updating ratings

--- hackathon_starter/hackathon/models.py
+++ hackathon_starter/hackathon/models.py
@@ -32,7 +32,7 @@ class Neighbourhood(models.Model):
         return self.name
 
 
-class Merchant(models.Model):
+class Merchant(OrigUser):
     area = models.CharField(max_length=140, default='Johannesburg')
     phone_number = models.CharField(max_length=140, default='000-000-0000')
     talent = models.CharField(max_length=140, default='')
@@ -44,13 +44,20 @@ class Merchant(models.Model):
     business_name = models.CharField(max_length=140, default='')
 
 
-class Ratings(models.Model):
+class Rating(models.Model):
     merchant = models.ForeignKey(Merchant)
     rating_ontime = models.IntegerField(default='-1')
     rating_value = models.IntegerField(default='-1')
     rating_reliability = models.IntegerField(default='-1')
     rating_quality = models.IntegerField(default='-1')
     user = models.ForeignKey(User)
+    text = models.TextField(max_length=200)
+
+    def __unicode__(self):
+        return "Merchant: " + self.merchant.name + " - Avg Rating: " + str(self.calculate_average())
+
+    def calculate_average(self):
+        return (self.rating_ontime + self.rating_quality + self.rating_reliability + self.rating_value) / 4.0
 
 
 class MerchantProfile(models.Model):
